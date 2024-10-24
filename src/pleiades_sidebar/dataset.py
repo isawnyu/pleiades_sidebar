8dbf545d7a86def2089aa54a45a4817106fedffc
Tom Elliott <ipse@paregorios.org>
2024-10-24 16:18:08 +0200

    sketch out base class for dataset and class for standard data item

--- src/pleiades_sidebar/dataset.py
+++ src/pleiades_sidebar/dataset.py
@@ -6,9 +6,62 @@
 #
 
 """
-Python 3 package template (changeme)
+Define a base class for a dataset manager
 """
+from pathlib import Path
 
-import logging
 
-logger = logging.getLogger(__name__)
+class DataItem:
+    """An individual data item in a dataset"""
+
+    def __init__(self, raw: dict):
+        self._ingest(raw)
+        self.label = None
+        self.uri = None
+        self.summary = None
+        self.links = dict()
+        self._raw_data = raw
+        # TBD: make spatial?
+
+    def _parse(self):
+        """Parse/ingest the raw data for this item into label, uri, and summary fields
+        OVERLOAD THIS METHOD for each dataset"""
+        # make sure you populate
+        # label
+        # uri
+        # summary
+        # links
+        pass
+
+
+class Dataset:
+    """Base class for a dataset manager"""
+
+    def __init__(self):
+        self._data = dict()  # Parsed DataItems keyed by URI
+        # Dictionary of lists of DataItem IDs keyed by Pleiades URIs
+        self._pleiades_index = dict()
+
+    def get(self, item_uri: str) -> DataItem:
+        """Get a parsed dataitem by its URI"""
+        try:
+            return self._data[item_uri]
+        except KeyError:
+            return None
+
+    def get_pleiades(self, pleiades_uri: str) -> list:
+        """Return a list of IDs for all items in this dataset that correspond to a Pleiades URI"""
+        try:
+            return self._pleiades_index[pleiades_uri]
+        except KeyError:
+            return list()
+
+    def load(self, datafile_path: Path, load_method: str):
+        """Load the target dataset"""
+        cmd = f"_load_{load_method}"
+        getattr(self, cmd)(datafile_path)
+
+    def parse_all(self):
+        """Parse the already-loaded dataset"""
+        # OVERRIDE THIS METHOD FOR EACH DATASET
+        pass
