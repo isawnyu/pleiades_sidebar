f5608e392da4a46cb8880c68b509f5f78dfeebfb
Tom Elliott <ipse@paregorios.org>
2024-10-25 14:50:45 +0200

    parse wikidata

--- src/pleiades_sidebar/dataset.py
+++ src/pleiades_sidebar/dataset.py
@@ -8,20 +8,23 @@
 """
 Define a base class for a dataset manager
 """
+from encoded_csv import get_csv
+import logging
 from pathlib import Path
+from pprint import pformat
 
 
 class DataItem:
     """An individual data item in a dataset"""
 
     def __init__(self, raw: dict):
-        self._ingest(raw)
         self.label = None
         self.uri = None
         self.summary = None
         self.links = dict()
         self._raw_data = raw
         # TBD: make spatial?
+        self._parse()
 
     def _parse(self):
         """Parse/ingest the raw data for this item into label, uri, and summary fields
@@ -60,8 +63,17 @@ class Dataset:
         """Load the target dataset"""
         cmd = f"_load_{load_method}"
         getattr(self, cmd)(datafile_path)
+        self.parse_all()
 
     def parse_all(self):
         """Parse the already-loaded dataset"""
         # OVERRIDE THIS METHOD FOR EACH DATASET
         pass
+
+    def _load_csv(self, datafile_path: Path):
+        data = get_csv(str(datafile_path), sample_lines=1000)
+        logger = logging.getLogger("_load_csv")
+        logger.debug(
+            f"Loaded {len(data['content'])} rows of data with fieldnames: {pformat(data['fieldnames'], indent=4)}"
+        )
+        self._raw_data = data["content"]
--- src/pleiades_sidebar/wikidata.py
+++ src/pleiades_sidebar/wikidata.py
@@ -11,13 +11,40 @@ Define a class for managing data from Wikidata
 import logging
 from os import environ
 from pathlib import Path
-from pleiades_sidebar.dataset import Dataset
+from pleiades_sidebar.dataset import Dataset, DataItem
+from pprint import pformat
+from textnorm import normalize_space, normalize_unicode
 
 logger = logging.getLogger("pleiades_sidebar")
 wikidata_path = Path(environ["WIKIDATA_PATH"]).expanduser().resolve()
 logger.debug(f"Wikidata path: {wikidata_path}")
 
 
+def norm(s: str) -> str:
+    return normalize_space(normalize_unicode(s))
+
+
 class WikidataDataset(Dataset):
     def __init__(self):
         Dataset.__init__(self)
+        Dataset.load(self, wikidata_path, "csv")
+
+    def parse_all(self):
+        for raw_item in self._raw_data:
+            wikidata_item = WikidataDataItem(raw_item)
+            logger.debug(pformat(wikidata_item, indent=4))
+            exit()
+
+
+class WikidataDataItem(DataItem):
+    def __init__(self, raw: dict):
+        DataItem.__init__(self, raw=raw)
+        self._raw_data = raw
+
+    def _parse(self):
+        self.label = norm(self._raw_data["itemLabel"])
+        self.uri = norm(self._raw_data["item"])
+        # label
+        # uri
+        # summary
+        # links
