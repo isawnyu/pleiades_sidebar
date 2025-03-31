5bda70d04af7355001795f284a655df81a6a2e38
Tom Elliott <ipse@paregorios.org>
2025-03-31 07:25:39 -0400

    update copyright statement

--- src/pleiades_sidebar/generator.py
+++ src/pleiades_sidebar/generator.py
@@ -1,7 +1,7 @@
 #
 # This file is part of pleiades_sidebar
 # by Tom Elliott for the Institute for the Study of the Ancient World
-# (c) Copyright 2024 by New York University
+# (c) Copyright 2024-2025 by New York University
 # Licensed under the AGPL-3.0; see LICENSE.txt file.
 #
 
